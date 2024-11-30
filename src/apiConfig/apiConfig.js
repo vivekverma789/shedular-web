7920629d5c03a5844be313091bc3bded8529ca66
Vivek Verma <vkverma4996@gmail.com>
2024-11-30 13:06:45 +0530

    Update apiConfig.js

--- src/apiConfig/apiConfig.js
+++ src/apiConfig/apiConfig.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 // Create an Axios instance with a base URL
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Replace with your API's base URL
+//  baseURL: "http://localhost:5000/api", // Replace with your API's base URL
+    baseURL: "https://shedular-backend.vercel.app/api",
+
 });
 
 export default api;
